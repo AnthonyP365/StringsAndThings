77ef0cc0cc17d674b9819ed6c8716e7dfcc2b07a
Anthony <145401343+AnthonyP365@users.noreply.github.com>
2024-03-08 08:20:19 -0500

    finished

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,9 @@
 package io.zipcoder;
 
 
+import static java.util.Collections.replaceAll;
+import static org.apache.maven.shared.utils.StringUtils.split;
+
 /**
  * @author tariq
  */
@@ -11,58 +14,99 @@ public class StringsAndThings {
      * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
-     *           countYZ("day fez"); // Should return 2
-     *           countYZ("day fyyyz"); // Should return 2
+     * countYZ("day fez"); // Should return 2
+     * countYZ("day fyyyz"); // Should return 2
      */
-    public Integer countYZ(String input){
-        return null;
+    public Integer countYZ(String input) {
+        int a = 0;
+        int b = 0;
+        String[] c = input.split(" ");
+        for (int i = 0; i < c.length; i++) {
+            if (c[i].charAt(c[i].length() - 1) == 'y') {
+                a++;
+            } else if (c[i].charAt(c[i].length() - 1) == 'z') {
+                b++;
+            }
+        }
+        return a + b;
     }
 
     /**
      * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
      * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
-     *
+     * <p>
      * example : removeString("Hello there", "llo") // Should return "He there"
-     *           removeString("Hello there", "e") //  Should return "Hllo thr"
-     *           removeString("Hello there", "x") // Should return "Hello there"
+     * removeString("Hello there", "e") //  Should return "Hllo thr"
+     * removeString("Hello there", "x") // Should return "Hello there"
      */
-    public String removeString(String base, String remove){
-        return null;
+    public String removeString(String base, String remove) {
+        return base.replace(remove, "");
     }
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
      * to the number of appearances of "not" anywhere in the string (case sensitive)
-     *
+     * <p>
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
-     *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
-     *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
+     * containsEqualNumberOfIsAndNot("This is notnot") // Should return true
+     * containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
-    public Boolean containsEqualNumberOfIsAndNot(String input){
-        return null;
+    public Boolean containsEqualNumberOfIsAndNot(String input) {
+        int is = 0;
+        int not = 0;
+
+        for (int i = 0; i <= input.length()-3; i++) {
+            if (input.substring(i, i + 2).equals("is")) {
+                is++;
+            } else if (input.substring(i, i + 3).equals("not")) {
+                not++;
+            }
+        }
+        return is == not;
     }
 
     /**
      * We'll say that a lowercase 'g' in a string is "happy" if there is another 'g' immediately to its left or right.
      * Return true if all the g's in the given string are happy.
      * example : gHappy("xxggxx") // Should return  true
-     *           gHappy("xxgxx") // Should return  false
-     *           gHappy("xxggyygxx") // Should return  false
+     * gHappy("xxgxx") // Should return  false
+     * gHappy("xxggyygxx") // Should return  false
      */
-    public Boolean gIsHappy(String input){
+    public Boolean gIsHappy(String input) {
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == 'g') {
+                if (i > 0 && input.charAt(i - 1) == 'g') {
+                    return true;
+                } else if (i > 0 && input.charAt(i + 1) == 'g') {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
         return null;
     }
 
 
-    /**
-     * We'll say that a "triple" in a string is a char appearing three times in a row.
-     * Return the number of triples in the given string. The triples may overlap.
-     * example :  countTriple("abcXXXabc") // Should return 1
-     *            countTriple("xxxabyyyycd") // Should return 3
-     *            countTriple("a") // Should return 0
-     */
-    public Integer countTriple(String input){
-        return null;
+        /**
+         * We'll say that a "triple" in a string is a char appearing three times in a row.
+         * Return the number of triples in the given string. The triples may overlap.
+         * example :  countTriple("abcXXXabc") // Should return 1
+         *            countTriple("xxxabyyyycd") // Should return 3
+         *            countTriple("a") // Should return 0
+         */
+        public Integer countTriple (String input){
+            int triple = 0;
+
+            for (int i = 0; i <= input.length()-3; i++) {
+                if (input.charAt(i) == input.charAt(i+1) && input.charAt(i) == input.charAt(i+2)) {
+                    triple++;
+                }
+            }
+            return triple;
+        }
     }
-}
+
+
